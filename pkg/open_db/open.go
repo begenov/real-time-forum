110bfff6112a90e8a0785d74779e589e19fc9cfe
begenov <begenovorazali@icloud.com>
2023-05-30 20:52:36 +0600

    update open db

--- pkg/open_db/open.go
+++ pkg/open_db/open.go
@@ -3,11 +3,12 @@ package opendbgo
 import (
 	"context"
 	"database/sql"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func OpenDB(ctx context.Context, driver string, dsn string) (*sql.DB, error) {
+func OpenDB(ctx context.Context, driver string, dsn string, path string) (*sql.DB, error) {
 	db, err := sql.Open(driver, dsn)
 	if err != nil {
 		return nil, err
@@ -16,5 +17,21 @@ func OpenDB(ctx context.Context, driver string, dsn string) (*sql.DB, error) {
 	if err := db.PingContext(ctx); err != nil {
 		return nil, err
 	}
+
+	if err = initTable(ctx, db, path); err != nil {
+		return nil, err
+	}
+
 	return db, nil
+}
+
+func initTable(ctx context.Context, db *sql.DB, path string) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	if _, err = db.ExecContext(ctx, string(data)); err != nil {
+		return err
+	}
+	return nil
 }
