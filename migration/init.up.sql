5ca11d007d0045ddb68f4552de071938d0fadcda
begenov <begenovorazali@icloud.com>
2023-05-30 21:43:40 +0600

    init migration

---
+++ migration/init.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nick_name VARCHAR(32) NOT NULL,
+    age INTEGER NOT NULL,
+    gender INTEGER NOT NULL, 
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    email VARCHAR(32) NOT NULL, 
+    password_hash VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS session (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL, 
+    token VARCHAR(64) NOT NULL,
+    expiration_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE 
+)
