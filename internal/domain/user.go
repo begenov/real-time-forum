39ee5f453a8e12f7e0df8c07b5dc47f8e638961f
begenov <begenovorazali@icloud.com>
2023-05-31 13:54:15 +0600

    update user domain

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,6 +1,7 @@
 package domain
 
 type User struct {
+	Id        int
 	Nickname  string
 	Age       int
 	Gender    int
@@ -8,4 +9,14 @@ type User struct {
 	LastName  string
 	Email     string
 	Password  string
+}
+
+type UserInput struct {
+	Nickname  string `json:"nickname"`
+	Age       int    `json:"age"`
+	Gender    int    `json:"gender"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
