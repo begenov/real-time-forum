5552579e76ed2a1c81757d54f2a3fca1d389149c
begenov <begenovorazali@icloud.com>
2023-05-31 13:50:40 +0600

    update app

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,9 +2,19 @@ package app
 
 import (
 	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/begenov/real-time-forum/internal/config"
+	delivery "github.com/begenov/real-time-forum/internal/delivery/http"
 	"github.com/begenov/real-time-forum/internal/repository"
+	"github.com/begenov/real-time-forum/internal/server"
+	"github.com/begenov/real-time-forum/internal/service"
+	"github.com/begenov/real-time-forum/pkg/auth"
+	"github.com/begenov/real-time-forum/pkg/hash"
 	opendb "github.com/begenov/real-time-forum/pkg/open_db"
 )
 
@@ -16,7 +26,38 @@ func Run(cfg *config.Config) error {
 		return err
 	}
 
+	hash := hash.NewHash(cfg.Hash.Cost)
+
+	manager := auth.NewManager()
+
 	repo := repository.NewRepository(db)
+
+	service := service.NewService(repo, hash, manager, cfg)
+
+	handler := delivery.NewHandler(service)
+
+	srv := server.NewServer(cfg, handler.Init())
+
+	go func() {
+		if err := srv.Run(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("error occurred while running http server: %s\n", err)
+		}
+	}()
+
+	log.Println("Server started", cfg.Server.Port)
+
+	quit := make(chan os.Signal, 1)
+
+	<-quit
+
+	const timeout = 5 * time.Second
+
+	ctx, shutdown := context.WithTimeout(context.Background(), timeout)
+	defer shutdown()
+
+	if err := srv.Stop(ctx); err != nil {
+		log.Fatalf("failed to stop server: %v", err)
+	}
 
 	return nil
 }
