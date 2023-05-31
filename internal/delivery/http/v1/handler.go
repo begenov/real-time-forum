4df5f4eaf8f7ec9f81e5f92b2c1bd34915f091e7
begenov <begenovorazali@icloud.com>
2023-05-31 13:52:36 +0600

    update handler

---
+++ internal/delivery/http/v1/handler.go
@@ -0,0 +1,13 @@
+package v1
+
+import "github.com/begenov/real-time-forum/internal/service"
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
