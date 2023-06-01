10c3afc589fcfabc77bb1ca4a285842415fe0980
begenov <begenovorazali@icloud.com>
2023-06-01 18:55:29 +0600

    init service

--- internal/service/service.go
+++ internal/service/service.go
@@ -18,12 +18,22 @@ type User interface {
 	DeleteSession(ctx context.Context, value string) error
 }
 
+type Post interface {
+	Create(ctx context.Context, post domain.Post) error
+	Update(ctx context.Context, post domain.Post) error
+	GetPostById(ctx context.Context, id int) (domain.Post, error)
+	GetAllPosts(ctx context.Context) ([]domain.Post, error)
+	Delete(ctx context.Context, id int) error
+}
+
 type Service struct {
 	User User
+	Post Post
 }
 
 func NewService(repo *repository.Repository, hash hash.PasswordHasher, manager auth.TokenManager, cfg *config.Config) *Service {
 	return &Service{
 		User: NewUserService(repo.Authorization, repo.Session, hash, manager, cfg.Token),
+		Post: NewPostService(repo.Post),
 	}
 }
