c4e84a78318804721c5f12cb12d707478f4be3be
begenov <begenovorazali@icloud.com>
2023-06-03 13:13:15 +0600

    rm

--- frontend/src/component/Log_out.js
+++
@@ -1,4 +0,0 @@
-export default function Log_Out() {
-    const divPage = document.createElement("div");
-    appDiv.appendChild(divPage);
-}
--- frontend/src/component/page/sign_in/sign_in.js
+++
@@ -1,5 +0,0 @@
-export default function Sign_In(appDiv) {
-    const divPage = document.createElement("div");
-    divPage.innerHTML = "<h1>Sign_in</h1>"
-    appDiv.appendChild(divPage);
-}
