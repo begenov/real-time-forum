2d854b3fad8e3dac83f9aa93c7327e831a54c606
begenov <begenovorazali@icloud.com>
2023-06-04 11:52:30 +0600

    update

--- internal/delivery/http/v1/handler.go
+++ internal/delivery/http/v1/handler.go
@@ -3,11 +3,13 @@ package v1
 import (
 	"github.com/begenov/real-time-forum/internal/service"
 	"github.com/begenov/real-time-forum/pkg/logger"
+	"github.com/gorilla/websocket"
 )
 
 type Handler struct {
-	service *service.Service
-	log     *logger.Log
+	service           *service.Service
+	log               *logger.Log
+	activeConnections []*websocket.Conn
 }
 
 func NewHandler(service *service.Service, log *logger.Log) *Handler {
