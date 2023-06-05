8478107476659af67a6e8dd7b44035f1c28f9c59
begenov <begenovorazali@icloud.com>
2023-06-05 13:13:29 +0600

    init cache

---
+++ pkg/cache/memory.go
@@ -0,0 +1,62 @@
+package cache
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var ErrItemNotFound = errors.New("cache: item not found")
+
+type item struct {
+	value    interface{}
+	createAt int64
+	ttl      int64
+}
+
+type MemoryCache struct {
+	cache map[interface{}]*item
+	sync.RWMutex
+}
+
+func NewMemoryCache() *MemoryCache {
+	c := &MemoryCache{cache: make(map[interface{}]*item)}
+	go c.setTtlTimer()
+
+	return c
+}
+
+func (c *MemoryCache) setTtlTimer() {
+	for {
+		c.Lock()
+		for k, v := range c.cache {
+			if time.Now().Unix()-v.createAt > -v.ttl {
+				delete(c.cache, k)
+			}
+		}
+		c.Unlock()
+		<-time.After(time.Second)
+	}
+}
+
+func (c *MemoryCache) Set(key, value interface{}, ttl int64) error {
+	c.Lock()
+	c.cache[key] = &item{
+		value:    value,
+		createAt: time.Now().Unix(),
+		ttl:      ttl,
+	}
+	c.Unlock()
+	return nil
+}
+
+func (c *MemoryCache) Get(key interface{}) (interface{}, error) {
+	c.RLock()
+	item, ok := c.cache[key]
+	c.RUnlock()
+	if !ok {
+		return nil, ErrItemNotFound
+	}
+
+	return item.value, nil
+}
