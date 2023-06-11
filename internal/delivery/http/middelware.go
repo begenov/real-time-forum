e77f61903acf9cdef75d66addae7decdf79e6641
begenov <begenovorazali@icloud.com>
2023-06-11 16:23:13 +0600

    update middelware

--- internal/delivery/http/middelware.go
+++ internal/delivery/http/middelware.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -20,6 +21,7 @@ func (h *Handler) setCorsHeaders(handler http.Handler) http.Handler {
 		handler.ServeHTTP(w, r)
 	})
 }
+
 func (h *Handler) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		h.log.Info(fmt.Sprintf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.String()))
@@ -32,4 +34,23 @@ func (h *Handler) logRequest(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (h *Handler) handleError(w http.ResponseWriter, statusCode int, err interface{}) {
+	h.log.Error(err)
+
+	errorResponse := map[string]interface{}{
+		"error": err,
+	}
+
+	h.writeJSONResponse(w, statusCode, errorResponse)
+}
+
+func (h *Handler) writeJSONResponse(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		h.handleError(w, http.StatusInternalServerError, "Failed to encode JSON response")
+		return
+	}
 }
